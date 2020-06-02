ae9a0516e930ac2142fc96ee820998a174436691
HungryOmnivore <43361567+HungryOmnivore@users.noreply.github.com>
2020-06-02 12:49:32 -0400

    Add files via upload

---
+++ .github/workflows/Account.java
@@ -0,0 +1,14 @@
+package com.omnivore.bank;
+
+public abstract class Account {
+	
+	//create methods for class Account in which you can Deposit and withdraw money, look up ID, back up to login, etc.
+	
+	Deposit
+	Withdraw
+	BackUp
+	LookUpID
+	
+	
+
+}
---
+++ .github/workflows/Driver.java
@@ -0,0 +1,10 @@
+package com.omnivore.bank;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
